64f41717b291f930eb283a14c65a9c604faed9d4
DevExpressExampleBot <fakeEmail>
2021-09-15 18:52:14 +0300

    Source auto update [skip ci]

--- VB/ViewModel/MainViewModel.vb
+++ VB/ViewModel/MainViewModel.vb
@@ -2,8 +2,8 @@
 Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace Example.ViewModel
-    <POCOViewModel> _
-    Public Class MainViewModel
-        Public Overridable Property SelectedText() As String
-    End Class
+	<POCOViewModel>
+	Public Class MainViewModel
+		Public Overridable Property SelectedText() As String
+	End Class
 End Namespace
