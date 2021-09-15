d892c713499f38cf8b447522ba9d07dc6b2adb13
Anthony Sam <anton.sameishchev@devexpress.com>
2021-09-15 19:05:51 +0300

    Update MainViewModel.cs

--- CS/ViewModel/MainViewModel.cs
+++ CS/ViewModel/MainViewModel.cs
@@ -1,8 +1,7 @@
 using DevExpress.Mvvm;
 
-namespace Example.ViewModel
-{
-    public class MainViewModel :ViewModelBase {
+namespace Example.ViewModel {
+    public class MainViewModel : ViewModelBase {
         public virtual string SelectedTextBoxText { get; set; }
     }
 }
